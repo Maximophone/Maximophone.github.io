c6fe6e4e8eb3ac49cc7e870fd93dabdba35157af
Maxime Fournes <maximefournes@Maximes-MacBook-Pro.local>
2019-01-24 19:13:54 +0000

    Added smooth camera size targetting

--- lazarus/scripts/index.js
+++ lazarus/scripts/index.js
@@ -303,11 +303,20 @@ class Camera{
     this.target = target
   }
   update(dt){
+	
     if(UserInputs.pressed_key(GameKeys.ZOOM_IN)){
       this.scale*=0.99
     } else if(UserInputs.pressed_key(GameKeys.ZOOM_OUT)){
       this.scale*=1.01
     }
+      var epsilon = 0.01
+      if(this.scale<this.target.size/20-epsilon){
+	  this.scale+=epsilon
+      }
+      else if (this.scale>this.target.size/20+epsilon){
+	  this.scale-=epsilon
+      }
+    //this.scale = this.target.size/20
     this.x = this.target.x - c.width/2*this.scale
     this.y = this.target.y - c.height/2*this.scale
   }
