275dedc3498abbaaa15a6373b25d29708c8805ec
Maxime Fournes <fournes.maxime@gmail.com>
2019-02-16 01:32:10 +0000

    Added eventlet monkeypatch, running on port 80 (needs sudo)

--- fiddling/app.py
+++ fiddling/app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 
--- fiddling/server.py
+++ fiddling/server.py
@@ -7,4 +7,4 @@ def start_game():
 
 if __name__ == "__main__":
     start_game()
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", port=80)
