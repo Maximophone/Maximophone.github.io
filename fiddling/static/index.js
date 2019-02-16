5dc24964ee4cf5b69a54ed92c9c13e6d6294c300
Maximux <fournes.maxime@gmail.com>
2019-02-16 01:12:39 +0000

    Added states per second display in client

--- fiddling/static/index.js
+++ fiddling/static/index.js
@@ -15,8 +15,37 @@ socket.on("ping", function(){
 })
 
 var global_state
+var prev_t
+var new_t
+var state_dt
+
+function fps_gen(name){
+    var last_dt = []
+    function fps(dt){
+	var N = 100
+	last_dt.push(dt)
+	if(last_dt.length >= N){
+	    var mean_dt = 0
+	    for(var dt of last_dt){
+		mean_dt += dt
+	    }
+	    mean_dt /= N
+	    console.log(name, ": ", 1000/mean_dt)
+	    last_dt = []
+	}
+    }
+    return fps
+}
+
+var fps = fps_gen("FPS")
+var sps = fps_gen("SPS")
+
 socket.on("state", function(state){
     global_state = state
+    prev_t = new_t
+    new_t = new Date().getTime()
+    state_dt = new_t - prev_t
+    sps(state_dt)
 })
 
 export var c = document.getElementById("c");
@@ -28,22 +57,6 @@ function draw(state){
 	var entities = state.entities
 	var particles = state.particles
 	painter.draw(camera, entities, particles)
-    }
-}
-
-var last_dt = []
-
-function fps(dt){
-    var N = 100
-    last_dt.push(dt)
-    if(last_dt.length >= N){
-	var mean_dt = 0
-	for(var dt of last_dt){
-	    mean_dt += dt
-	}
-	mean_dt /= N
-	console.log("FPS: ", 1000/mean_dt)
-	last_dt = []
     }
 }
 
